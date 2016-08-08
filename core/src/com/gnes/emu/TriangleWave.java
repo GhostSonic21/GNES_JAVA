bb43acbb25180fa8656f5f6b30a55c430885b8c8
GhostSonic <ghostsonic21@gmail.com>
2016-08-08 13:35:04 -0700

    Flip around triangle wave sequence table
    So it starts from 0-15 instead of the other way around.

--- core/src/com/gnes/emu/TriangleWave.java
+++ core/src/com/gnes/emu/TriangleWave.java
@@ -23,9 +23,10 @@ public class TriangleWave implements WaveChannel {
     private boolean trianglePopHack = true;
 
     // Sequence lookup table
+    // Hopefully this is right (doesn't seem to matter if it's 0-15 or 15-0)
     private final int[] sequenceTable = {
-        15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
     };
 
 
